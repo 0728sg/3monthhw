c88e7b58da976615b8dd2d59c158bea71b71e0f2
Cheremuxa <gaukhar.2006@bk.ru>
2024-09-10 18:10:47 +0600

    obnova

--- buttons.py
+++ buttons.py
@@ -7,6 +7,8 @@ start_buttons = KeyboardButton('/start')
 mem_buttons = KeyboardButton('/mem')
 mem_all_buttons = KeyboardButton('/mem_all')
 music_buttons = KeyboardButton('/music')
+quiz_buttons = KeyboardButton('/quiz')
+game_buttons = KeyboardButton('/game')
 
 
 start.add(start_buttons, mem_buttons, mem_all_buttons,
@@ -20,7 +22,9 @@ start_test = ReplyKeyboardMarkup(
 KeyboardButton('/start'),
     KeyboardButton('/mem'),
     KeyboardButton('/mem_all'),
-    KeyboardButton('/music')
+    KeyboardButton('/music'),
+    KeyboardButton('/quiz'),
+    KeyboardButton('/game')
 )
 
 start_test_1 = ReplyKeyboardMarkup(
@@ -32,5 +36,7 @@ start_test_1.add(
     KeyboardButton('/start'),
     KeyboardButton('/mem'),
     KeyboardButton('/mem_all'),
-    KeyboardButton('/music')
+    KeyboardButton('/music'),
+    KeyboardButton('/quiz'),
+    KeyboardButton('/game')
 )
